c586f68d5a22ea79ba8ee4ba2e280bf4a484b355
nehayaligar <nehayaligar03@gmail.com>
2025-04-29 09:33:27 +0530

    saved

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -6,11 +6,15 @@ public class EmployeeDetails {
     private int id;
     private String name;
     private double salary;
+    private String location;
+    private String Department;
    
-    public EmployeeDetails(int id, String name, double salary) {
+    public EmployeeDetails(int id, String name, double salary, String location, String Department) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.location=location;
+        this.Department=Department;
         
 
     }
@@ -19,10 +23,12 @@ public class EmployeeDetails {
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Employee Salary: " + salary);
+        System.out.println("Employee Location: " + location);
+        System.out.println("Employee Department: " + Department);
        
     }
     public static void main(String[] args) {
-        EmployeeDetails employee = new EmployeeDetails(101, "Alaina", 50000.00);
+        EmployeeDetails employee = new EmployeeDetails(101, "Alaina", 50000.00,"Hubli","Computer Science");
         employee.displayDetails();
     }
 }
